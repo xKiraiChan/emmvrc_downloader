ae0ab5cf4f18ee245ead859d78c9bb9ba2dc8d9d
xKiraiChan <xKiraiChan@gmail.com>
2021-04-23 23:17:18 -0400

    update for new endpoint

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ async fn main() {
     std::fs::File::create("./emmVRC.dll").unwrap()
     .write_all(
         &base64::decode(
-            &reqwest::get("https://thetrueyoshifan.com/BakaUpdate.php?libdownload")
+            &reqwest::get("https://dl.emmvrc.com/update.php?libdownload")
                 .await
                 .expect("Downloading")
                 .bytes()
